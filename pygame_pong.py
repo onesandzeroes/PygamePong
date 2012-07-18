8cfc941975044a705f4a043ef55bf9d19be742d5
onesandzeroes <marius.mather@gmail.com>
2012-07-18 20:43:52 +1000

    Added score Score is now printed to the screen, and updated when a player scores a point

--- pygame_pong.py
+++ pygame_pong.py
@@ -39,7 +39,7 @@ class Ball:
     def start(self):
         self.rect = (self.xsize / 2, self.ysize / 2, 5, 5)
         start_speed = random.choice([1, -1])
-        self.xspeed = 0.2 * start_speed
+        self.xspeed = 0.4 * start_speed
         self.yspeed = 0
     def move(self):
         x, y, width, height = self.rect
@@ -58,6 +58,7 @@ class Screen:
         self.screen = pygame.display.set_mode((500, 500))
         self.background = pygame.Surface(self.screen.get_size())
         self.background = self.background.convert()
+        self.font = pygame.font.Font(None, 30)
         self.ball = Ball(self.xsize, self.ysize)
         self.player1 = Player('L', 1)
         self.player2 = Player('R', 2)
@@ -85,6 +86,11 @@ class Screen:
         # Draw the player paddle
         pygame.draw.rect(self.screen, WHITE, self.player1.rect)
         pygame.draw.rect(self.screen, WHITE, self.player2.rect)
+        # Player scores
+        score1_text = self.font.render(str(self.player1.score), True, WHITE)
+        score2_text = self.font.render(str(self.player2.score), True, WHITE)
+        self.screen.blit(score1_text, (self.xsize * 0.25, 10))
+        self.screen.blit(score2_text, (self.xsize * 0.75, 10))
         pygame.display.update()
     def collision_check(self):
         ballx, bally, ballw, ballh = self.ball.rect
@@ -101,10 +107,10 @@ class Screen:
                 self.ball.xspeed *= -1
                 self.angle(self.player2)
         elif ballx <= 0:
-            print('Player 2 scores!')
+            self.player2.score += 1
             self.ball.start()
         elif ballx >= self.xsize:
-            print('Player 1 scores!')
+            self.player1.score += 1
             self.ball.start()
         elif bally <= 0 or bally >= self.ysize:
             self.ball.yspeed *= -1
@@ -115,7 +121,6 @@ class Screen:
         pcenter = (py + py + ph) / 2
         # This should scale the distance according to the paddle height
         d_from_center = abs(bally - pcenter)/(ph / 2)
-        print(d_from_center)
         self.ball.yspeed += d_from_center * 0.2
 
 
